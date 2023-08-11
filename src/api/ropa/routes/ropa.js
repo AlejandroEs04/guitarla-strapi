1840e246d3e0c472f7c3cfdd0865bad689b8f5bf
Alexanderel04 <2004.estrada.lopez@gmail.com>
2023-08-11 15:33:23 -0600

    "Upload 11/08/2023"

---
+++ src/api/ropa/routes/ropa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ropa router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ropa.ropa');
---
+++ src/api/ropa/services/ropa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ropa service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ropa.ropa');
